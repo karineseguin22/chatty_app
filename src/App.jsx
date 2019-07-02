ebfcdbc1bf7b7c0b55e5448033db15bf33d9793d
karineseguin22 <karine.seguin2@mail.mcgill.ca>
2019-07-02 22:22:21 +0000

    Add data in App class in this.state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import React, {Component} from 'react';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+  messages: [
+    {
+      username: "Bob",
+      content: "Has anyone seen my marbles?",
+    },
+    {
+      username: "Anonymous",
+      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+    }
+  ]
+    }; 
+  }
   render() {
     return (
       <Body>
-        
+
       </Body>
     );
   }
