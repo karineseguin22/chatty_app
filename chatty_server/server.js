9f323f8a09d2e1222b9873bdac80d79e655c0b07
karineseguin22 <karine.seguin2@mail.mcgill.ca>
2019-07-04 15:28:04 +0000

    Comment out id in client side because need to be in server side instead

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -21,6 +21,9 @@ wss.on('connection', (ws) => {
   ws.on('open', () => console.log('Client Connected'));
   ws.on('message', function incoming(data) {
     console.log(JSON.parse(data))
+    //need to add id to message 
+    //broadcast message to all clients
+    
   })
   // Set up a callback for when a Client closes the socket. This usually means they closed their browser.
   ws.on('close', () => console.log('Client disconnected'));
